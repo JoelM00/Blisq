452ba008a672e5e911ce746c35f21a86272b09af
Joel Martins <61049184+JoelM00@users.noreply.github.com>
2022-07-15 12:27:37 +0100

    update

--- Inopla/js/scripts.js
+++ Inopla/js/scripts.js
@@ -1,25 +1,15 @@
 $(document).ready(function() {
-	AOS.init({
-		once: true,
-	});
-	if(!$("body").hasClass("home")) {
-		if($(window).width() > 1280) {
-			$('.top-menu').toggleClass('active');
-		}
-	}
 
-	$('.btn-menu').on('click', function(){
-		$('.menu-list').toggleClass('menu-list-close');
-	});
-	$('.menu-button').on('click', function(){
-		$('.top-menu').toggleClass("active")
-	});
 	$('.menu-button-close').on('click', function(){
-		$('.top-menu').toggleClass("active")
+		$('.menu').toggleClass("active")
 	});
 
+	$('.menu-button-open').on('click', function(){
+		$('.menu').toggleClass("active")
+	});
+	
 
-	$('.home-solutions-showcase-grid').slick(
+	$('.slider').slick(
 		{
 			slidesToShow: 4,
 			slidesToScroll: 1,
@@ -29,8 +19,8 @@ $(document).ready(function() {
 			arrows: true,
 			adaptiveHeight: false,
 			dots: false,
-			prevArrow: $('.home-showcase-arrow-left'),
-			nextArrow: $('.home-showcase-arrow-right'),
+			prevArrow: $('.arrow-left'),
+			nextArrow: $('.arrow-right'),
 			infinite:true,
 			responsive: [
 				{
@@ -56,44 +46,6 @@ $(document).ready(function() {
 				}
 			]
 		}
-
 	)
-	$('.about-cta3-gallery').slick(
-		{
-			slidesToShow: 2,
-			slidesToScroll: 1,
-			autoplay: false,
-			autoplaySpeed: 2000,
-			arrows: true,
-			dots: false,
-			infinite: false,
-			prevArrow: $('.cta3-gallery-arrow-left'),
-			nextArrow: $('.cta3-gallery-arrow-right')
-		}
-
-	)
-	$('.solution-product-gallery').masonry({
-		// options
-		itemSelector: '.solution-product-gallery-image',
-		columnWidth: '.solution-product-gallery-image',
-		percentPosition: true,
-		gutter: 10,
-	});
-
-	$video = $('.solution-product-video');
-	$button = $('#play-button')
-	if($video) {
-		$('.solution-product-video, #play-button').on('click', function() {
-			if($video.get(0).paused) {
-				$video.get(0).play();
-				$button.addClass('play-button-pause');
-				$button.attr('src',`${theme.assets}/assets/images/svg/pause-button.svg`);
-			} else {
-				$video.get(0).pause();
-				$button.removeClass('play-button-pause');
-				$button.attr('src',`${theme.assets}/assets/images/play-button.png`);
-			}
-		});
-	}
 });
 
