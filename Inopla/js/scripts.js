140c46c896e621af6116e2b0eddbd6351db7593f
Joel Martins <61049184+JoelM00@users.noreply.github.com>
2022-07-18 17:34:04 +0100

    update

--- Inopla/js/scripts.js
+++ Inopla/js/scripts.js
@@ -9,64 +9,34 @@ $(document).ready(function() {
 	});
 	
 
-	$('.slider').slick(
-		{
-			slidesToShow: 4,
-			slidesToScroll: 1,
-			autoplay: true,
-			autoplaySpeed: 1000,
-			speed: 200,
-			arrows: true,
-			adaptiveHeight: false,
-			dots: false,
-			prevArrow: $('.arrow-left'),
-			nextArrow: $('.arrow-right'),
-			infinite:true,
-			responsive: [
-				{
-					breakpoint: 1441,
-					settings: {
-						slidesToShow: 2,
-						slidesToScroll: 1,
-						infinite: false,
-						dots: false,
-						arrows: true,
-					}
-				},
-				{
-					breakpoint: 701,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1,
-						infinite: false,
-						dots: false,
-						arrows: true,
-					}
-
-				}
-			]
-		}
-	)
-
-	$('.about-cta3-gallery').slick(
-		{
-			slidesToShow: 2,
-			slidesToScroll: 1,
-			autoplay: false,
-			autoplaySpeed: 2000,
-			arrows: true,
-			dots: false,
-			infinite: false,
-			prevArrow: $('.cta3-gallery-arrow-left'),
-			nextArrow: $('.cta3-gallery-arrow-right')
-		}
-	)
-
-	$('.content-gallery').masonry({
-		itemSelector: '.content-gallery-image',
-		columnWidth: '.content-gallery-image',
-		percentPosition: true,
-		gutter: 10,
+	$('.solutions-slider').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		speed: 200,
+		arrows: true,
+		dots: false,
+		prevArrow: $('.arrow-left'),
+		nextArrow: $('.arrow-right'),
 	});
+			  
+				  
+	$('.sub-about-gallery').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: false,
+		prevArrow: $('.arrow-left'),
+		nextArrow: $('.arrow-right'),
+	});
+
+	$('.grid').masonry({
+		// options
+		itemSelector: '.grid-item',
+		columnWidth: 200,
+		gutter: 20,
+	  });
 });
 
