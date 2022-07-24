a86a1f50abec2cc4eb9975c2cc701a5b843ebaf2
Joel Martins <61049184+JoelM00@users.noreply.github.com>
2022-07-24 10:03:12 +0100

    100%

--- React/dojo-blog/src/BlogDetails.js
+++ React/dojo-blog/src/BlogDetails.js
@@ -1,15 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id)
-    
-    const handleClick = () => {
+    const history = useHistory()
+
+    const handleDelete = () => {
         fetch('http://localhost:8000/blogs/' + blog.id,{
             method: 'DELETE',
         }).then(() => {
-            
+            history.push('/')
         })
     }
 
@@ -22,7 +23,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{ blog.body }</div>
-                    <button onClick={handleClick}>delete</button>
+                    <button onClick={handleDelete}>delete</button>
                 </article>
             )}
         </div>
